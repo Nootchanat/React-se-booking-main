7a08087a9166803b143b15578756688b791579ba
Nootchanat <nootchanatlees@gmail.com>
2023-03-20 00:12:58 +0700

    Booking

---
+++ src/components/HotelItem.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const HotelItem = () => {
+  return (
+    <div className="hotels">
+
+       <div className="hotelsItem">
+      <img
+        src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707367.jpg?k=cbacfdeb8404af56a1a94812575d96f6b80f6740fd491d02c6fc3912a16d8757&o=&hp=1"
+        className="hotelImg"
+      />
+
+      </div>
+       <div className="hotelsItem">
+      <img
+        src="https://images.unsplash.com/photo-1549294413-26f195200c16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
+        className="hotelImg"
+      />
+
+      </div>
+       <div className="hotelsItem">
+      <img
+        src="https://pix10.agoda.net/hotelImages/21985765/0/b4e6ee12239da94e4e6b6c292b9da629.jpg?ca=17&ce=1&s=1024x768"
+        className="hotelImg"
+      />
+
+      </div>
+      <div className="hotelsItem">
+      <img
+        src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/253380625.jpg?k=b18b36259ed24c7c61d398915914d72424353f7606a8fc7eec5fe3b2f17bb352&o=&hp=1"
+        className="hotelImg"
+      />
+
+      </div>
+
+      <div className="hotelsItem">
+      <img
+        src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1"
+        className="hotelImg"
+      />
+
+      </div>
+      <div className="hotelsItem">
+      <img
+        src="https://images.unsplash.com/photo-1596309954454-f70b8239b4f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80"
+        className="hotelImg"
+      />
+
+      </div>
+
+
+      </div>
+
+
+
+  )
+}
+
+export default HotelItem
